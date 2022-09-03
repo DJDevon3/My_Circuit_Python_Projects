7b4ebe9df1c70084493638a1ddcb066beb78cc8f
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-09-03 12:53:58 -0400

    Add files via upload

---
+++ Boards/espressif/Adafruit Magtag 2.9 Grayscale/MagTag Daily Weather Forecast/secrets.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'your wifi SSID',
+    'password' : 'your wifi password',
+    'aio_username' : 'adafruitio username',
+    'aio_key' : 'adafruitio token',
+    'timezone' : "America/New_York",  # http://worldtimeapi.org/timezones
+    'openweather_token' : 'your openweathermap token',
+    'openweather_location' : 'Philadelphia, PA',
+    }
+
