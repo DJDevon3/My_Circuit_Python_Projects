3e57d7dec74b7119d0fecadbe09c3b78563429b3
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-11-10 13:05:59 -0500

    Updated with Neradoc latch & asyncio

--- Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2/code.py
+++ Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2/code.py
@@ -1,7 +1,8 @@
 # DJDevon3 TR-Cowbell Hardware Test
-# 16 Sept 2022 - DJDevon3
+# 2022/11/09 - Neradoc & DJDevon3
 # Based on PicoStepSeq by @todbot Tod Kurt
 # https://github.com/todbot/picostepseq/
+import asyncio
 import time
 import board
 import busio
@@ -14,50 +15,46 @@ from multi_macropad import MultiKeypad
 # Initialize 2 Separate Physical I2C busses
 i2c0 = busio.I2C(board.GP13, board.GP12)  # Bus I2C0
 i2c1 = busio.I2C(board.GP11, board.GP10)  # Bus I2C1
+
 # Initialize MCP Chip 1 Step Switches 0-7
-mcp = MCP23017(i2c0, address=0x26)
+mcp1 = MCP23017(i2c0, address=0x26)
 # Initalize MCP Chip 2 Step Switches 8-15
 mcp2 = MCP23017(i2c1, address=0x20)
 
-PINS = [0, 1, 2, 3, 4, 5, 6, 7]
+PINS1 = [0, 1, 2, 3, 4, 5, 6, 7]
 PINS2 = [0, 1, 2, 3, 4, 5, 6, 7]
-# Neradoc's MCPMatrixScanner
-scanner = McpKeysScanner(mcp, PINS)
+
+# MCP scanner and multikeypad
+scanner1 = McpKeysScanner(mcp1, PINS1)
 scanner2 = McpKeysScanner(mcp2, PINS2)
-all_scanner = MultiKeypad(scanner, scanner2)
+all_scanner = MultiKeypad(scanner1, scanner2)
 
-# MCP1 PORT A SWITCHES (0-7)
-chip1_port_a_pins = []
-for pin in range(0, 8):
-    chip1_port_a_pins.append(mcp.get_pin(pin))
+# LED pins on ports B
+mcp1_led_pins = [mcp1.get_pin(pin) for pin in range(8, 16)]
+mcp2_led_pins = [mcp2.get_pin(pin) for pin in range(8, 16)]
 
-# MCP1 PORT B LEDS (8-15)
-chip1_port_b_pins = []
-for pin in range(8, 16):
-    chip1_port_b_pins.append(mcp.get_pin(pin))
+# all the LED pins organized per MCP chip
+led_pins_per_chip = (mcp1_led_pins, mcp2_led_pins)
 
-# MCP2 PORT A SWITCHES (0-7)
-chip2_port_a_pins = []
-for pin in range(0, 8):
-    chip2_port_a_pins.append(mcp2.get_pin(pin))
+# ordered list of led coordinates
+led_pins = [(a, b) for a in range(2) for b in range(8)]
 
-# MCP2 PORT B LEDS (8-15)
-chip2_port_b_pins = []
-for pin in range(8, 16):
-    chip2_port_b_pins.append(mcp2.get_pin(pin))
+# Set all LED pins to output
+for (m,x) in led_pins:
+    led_pins_per_chip[m][x].direction = Direction.OUTPUT
 
-# Combine all pins of same type into 1 list
-port_a_pins = chip1_port_a_pins + chip2_port_a_pins
-port_b_pins = chip1_port_b_pins + chip2_port_b_pins
+# status of the button latches
+latches = [False] * 16
+def toggle_latch(mcp, pin):
+    print(mcp, pin)
+    latches[mcp * 8 + pin] = not latches[mcp * 8 + pin]
+def get_latch(mcp, pin):
+    return latches[mcp * 8 + pin]
 
-# Set all Port A Switch pins to input, with pullups!
-for pin in port_a_pins:
-    pin.direction = Direction.INPUT
-    pin.pull = Pull.UP
-
-# Set all Port B LED pins to output
-for pin in port_b_pins:
-    pin.direction = Direction.OUTPUT
+# NOTE: it is assumed that key number x (port A) on MCP number y matches
+# the LED numnber x (port B) on the same MCP number y
+# if not, a conversion function could be used to translate:
+# (key_x, key_y) -> (led_x, led_y)
 
 # midi setup
 midi_tx_pin, midi_rx_pin = board.GP16, board.GP17
@@ -65,24 +62,42 @@ midi_timeout = 0.01
 uart = busio.UART(tx=midi_tx_pin, rx=midi_rx_pin,
                   baudrate=31250, timeout=midi_timeout)
 
-while True:
-    for key, led in zip(port_a_pins, port_b_pins):
-        led.switch_to_output(value=True)
-        led.switch_to_output(value=False)
-        time.sleep(.125)
+async def blink_the_leds(delay=0.125):
+    while True:
+        # blink all the LEDs together
+        for (x,y) in led_pins:
+            if not get_latch(x, y):
+                led_pins_per_chip[x][y].value = True
+                time.sleep(0.001)
+                led_pins_per_chip[x][y].value = False
+                await asyncio.sleep(delay)
 
-        scanner.update()
+async def read_buttons():
+    while True:
+        # scan the buttons
+        scanner1.update()
         scanner2.update()
+        # treat the events
         while event := all_scanner.next_event():
-            key = event.key_number
+            mcp_number = event.pad_number
+            key_number = event.key_number
             if event.pressed:
-                print(f"Key pressed : {key}")
-                led.switch_to_output(value=True)
-                latched = True
-            if event.released & latched:
-                combine_led = event.key_number+8  # correct key_number for matching LED
-                print(f"Key latched : {key} {combine_led}")
-                print(led)
-                latched = True
-            if event.released:
-                print(f"Key released: {key}")
+                print(f"Key pressed : {mcp_number} / {key_number}")
+                # key pressed, find the matching LED
+                led_pin = led_pins_per_chip[mcp_number][key_number]
+                # invert the latch value (independently of the LED)
+                toggle_latch(mcp_number, key_number)
+                # change the LED value to match the latch
+                led_pin.value = get_latch(mcp_number, key_number)
+            # make sure to yield during the reading of the buttons
+            await asyncio.sleep(0)
+        # slow down the loop a little bit, can be adjusted
+        await asyncio.sleep(0.005)
+
+async def main():
+    await asyncio.gather(
+        asyncio.create_task(blink_the_leds()),
+        asyncio.create_task(read_buttons()),
+    )
+
+asyncio.run(main())
