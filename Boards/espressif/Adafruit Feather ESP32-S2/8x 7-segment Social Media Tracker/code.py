bf43646f6066a280f6635122374a8c1904900a07
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-11-15 06:42:48 -0500

    Added Twitch with 0auth

--- Boards/espressif/Adafruit Feather ESP32-S2/8x 7-segment Social Media Tracker/code.py
+++ Boards/espressif/Adafruit Feather ESP32-S2/8x 7-segment Social Media Tracker/code.py
@@ -18,9 +18,9 @@ import re
 from analogio import AnalogOut
 from adafruit_ht16k33 import segments
 
-# Initialize WiFi Pool (This should always be near the top of a script!)
-# anecdata: you only want to do this once early in your code pool.
-# Highlander voice: "There can be only one pool"
+Twitch_UserID = "0000000"
+
+# Initialize WiFi Pool (There can be only one pool)
 pool = socketpool.SocketPool(wifi.radio)
 
 # Initialize 7-Segment Backpack
@@ -62,19 +62,27 @@ try:
 except ImportError:
     print("Secrets File Import Error")
     raise
+    
+# Converts seconds in minutes/hours/days
+def time_calc(input_time):
+    if input_time < 60:
+        sleep_int = input_time
+        time_output = f"{sleep_int:.0f} seconds"
+    elif 60 <= input_time < 3600:
+        sleep_int = input_time / 60
+        time_output = f"{sleep_int:.0f} minutes"
+    elif 3600 <= input_time < 86400:
+        sleep_int = input_time / 60 / 60
+        time_output = f"{sleep_int:.0f} hours"
+    elif 86400 <= input_time < 432000:
+        sleep_int = input_time / 60 / 60 / 24
+        time_output = f"{sleep_int:.1f} days"
+    else:  # if > 5 days convert float to int & display whole days
+        sleep_int = input_time / 60 / 60 / 24
+        time_output = f"{sleep_int:.0f} days"
+    return time_output
 
-if sleep_time < 60:
-    sleep_time_conversion = "seconds"
-    sleep_int = sleep_time
-elif 60 <= sleep_time < 3600:
-    sleep_int = sleep_time / 60
-    sleep_time_conversion = "minutes"
-elif 3600 <= sleep_time < 86400:
-    sleep_int = sleep_time / 60 / 60
-    sleep_time_conversion = "hours"
-else:
-    sleep_int = sleep_time / 60 / 60 / 24
-    sleep_time_conversion = "days"
+print("Calc Time: ", time_calc(900))  # time conversion testing
 
 # https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=[YOUR_USERNAME]&key=[YOUR_API_KEY]
 YT_SOURCE = (
@@ -86,6 +94,7 @@ YT_SOURCE = (
     + secrets["YT_token"]
 )
 
+# TWITTER
 # Used with any Twitter 0auth request. Twitter developer account bearer token required.
 twitter_header = {'Authorization': 'Bearer ' + secrets["TW_bearer_token"]}
 # Twitter UserID is a number.
@@ -94,6 +103,13 @@ TW_SOURCE = (
     "https://api.twitter.com/2/users/"
     + secrets["TW_userid"]
     + "?user.fields=public_metrics,created_at,pinned_tweet_id&expansions=pinned_tweet_id&tweet.fields=created_at,public_metrics,source,context_annotations,entities"
+)
+
+# First we use Client ID & Client Secret to create a token with POST
+# You must be logged into Twitch when you do this.
+twitch_0auth_header = {'Content-Type': 'application/x-www-form-urlencoded'}
+TWITCH_0AUTH_TOKEN = (
+    "https://id.twitch.tv/oauth2/token"
 )
 
 github_header = {'Authorization':' token ' + secrets["Github_token"]}
@@ -130,6 +146,7 @@ print("Connected!\n")
 
 while True:
     try:
+        gc.collect()
         print("Attempting to GET YouTube Stats!")  # ------------------------------------------------
         # Uncomment line below to print API URL with all data filled in
         # print("Full API GET URL: ", YT_SOURCE)
@@ -139,37 +156,37 @@ while True:
         except ConnectionError as e:
             print("Connection Error:", e)
             print("Retrying in 10 seconds")
-            
+
         # Print Full JSON to Serial
         full_yt_json_response = False # Change to true to see full response
         if full_yt_json_response:
             dump_object = json.dumps(response)
             print("JSON Dump: ", dump_object)
-            
+
         # Print Debugging to Serial
         yt_debug = True # Set to True to print Serial data
         if yt_debug:
             print("Matching Results: ", response['pageInfo']['totalResults'])
-            
+
             YT_response_channel_kind = response['kind']
             print("Response Kind: ", YT_response_channel_kind)
-            
+
             YT_response_channel_kind = response['items'][0]['kind']
             print("Request Kind: ", YT_response_channel_kind)
-            
+
             YT_response_channel_id = response['items'][0]['id']
             print("Channel ID: ", YT_response_channel_id)
-            
+
             YT_response_channel_videoCount = response['items'][0]['statistics']['videoCount']
             print("Videos: ", YT_response_channel_videoCount)
-            
+
             YT_response_channel_viewCount = response['items'][0]['statistics']['viewCount']
             print("Views: ", YT_response_channel_viewCount)
-            
+
             YT_response_channel_subscriberCount = response['items'][0]['statistics']['subscriberCount']
             print("Subscribers: ", YT_response_channel_subscriberCount)
             print("Monotonic: ", time.monotonic())
-            
+
         # YouTube 7-Segment Display
         red.brightness = 0.8
         red.fill(0)
@@ -209,7 +226,7 @@ while True:
         twitter_follower_count = twitter_json['public_metrics']['followers_count']
         print("Followers: ", twitter_follower_count)
         print("Monotonic: ", time.monotonic())
-        
+
         # Twitter 7-Segment Display
         blue.brightness = 0.8
         blue.fill(0)
@@ -224,18 +241,98 @@ while True:
         blue.fill(0)
         blue.print(twitter_follower_count)
         
+        gc.collect()
+        print("\nAttempting to GENERATE Twitch Bearer Token!")  # ------------------------------------------------
+        print("===============================")
+        twitch_0auth_data = ("&client_id="
+        + secrets["Twitch_ClientID"]
+        + "&client_secret="
+        + secrets["Twitch_Client_Secret"]
+        + "&grant_type=client_credentials"
+        )
+    
+        twitch_0auth_response = requests.post(url=TWITCH_0AUTH_TOKEN, data=twitch_0auth_data, headers=twitch_0auth_header)
+        try:
+            twitch_0auth_json = twitch_0auth_response.json()
+        except ConnectionError as e:
+            print("Connection Error:", e)
+            print("Retrying in 10 seconds")
+        # Uncomment line below to print API URL you're sending to Twitter
+        # print("Full API GET URL: ", TWITCH_0AUTH_TOKEN)
+        # print("JSON Dump: ", twitch_0auth_json)
+        twitch_access_token = twitch_0auth_json['access_token']
+        # print("Access Token: ", twitch_access_token)
+        twitch_token_expiration = twitch_0auth_json['expires_in']
+        print("Token Expires in: ", time_calc(twitch_token_expiration))
+        
+        twitch_token_type = twitch_0auth_json['token_type']
+        print("Token Type: ", twitch_token_type)
+        print("Monotonic: ", time.monotonic())
+
+        # Twitch 7-Segment Display
+        blue.brightness = 0.8
+        blue.fill(0)
+        blue.print("twitch")
+        time.sleep(2)
+        blue.fill(0)
+        blue.print("token")
+        time.sleep(2)
+        blue.fill(0)
+        blue.print(twitch_token_type)
+        
+        # Helix is the name of the current Twitch API
+        # Twitch API versioning uses names not numbers
+        # Recommend for finding your User ID
+        # https://www.streamweasels.com/tools/convert-twitch-username-to-user-id/
+        twitch_header = {
+        'Authorization': 'Bearer '+twitch_access_token+'',
+        'Client-Id': ''+ secrets["Twitch_ClientID"] +''
+        }
+        TWITCH_FOLLOWERS_SOURCE = (
+            "https://api.twitch.tv/helix/users"
+            + "/follows?"
+            + "to_id="
+            + Twitch_UserID
+            + "&first=1"
+        )
+        print("\nAttempting to GET TWITCH Stats!")  # ------------------------------------------------
+        print("===============================")
+        twitch_followers_response = requests.get(url=TWITCH_FOLLOWERS_SOURCE, headers=twitch_header)
+        try:
+            twitch_followers_json = twitch_followers_response.json()
+        except ConnectionError as e:
+            print("Connection Error:", e)
+            print("Retrying in 10 seconds")
+        # Uncomment line below to print API URL you're sending to Twitter
+        # print("Full API GET URL: ", TWITCH_FOLLOWERS_SOURCE)
+        # print("Header: ", twitch_header)
+        # print("JSON Full Response: ", twitch_followers_json)
+        twitch_username = twitch_followers_json['data'][0]['to_name']
+        print("Username: ", twitch_username)
+        twitch_followers = twitch_followers_json['total']
+        print("Followers: ", twitch_followers)
+            
+        # Twitch 7-Segment Display
+        blue.brightness = 0.8
+        blue.fill(0)
+        blue.print("subs")
+        time.sleep(2)
+        blue.fill(0)
+        blue.print(twitch_followers)
+        print("Monotonic: ", time.monotonic())
+        
         print("\nAttempting to GET GITHUB Stats!")  # ------------------------------------------------
         # Uncomment line below to print API URL with all data filled in
-        print("Full API GET URL: ", GH_SOURCE)
+        # print("Full API GET URL: ", GH_SOURCE)
         print("===============================")
         github_response = requests.get(url=GH_SOURCE, headers=github_header).json()
-        
+
         # Print Full JSON to Serial
         full_github_json_response = False # Change to true to see full response
         if full_github_json_response:
             dump_object = json.dumps(github_response)
             print("JSON Dump: ", dump_object)
-            
+
         # Print Debugging to Serial
         github_debug = True # Set to True to print Serial data
         if github_debug:
@@ -246,7 +343,7 @@ while True:
             github_followers = github_response['followers']
             print("Followers: ", github_followers)
         print("Monotonic: ", time.monotonic())
-        
+
         # Github 7-Segment Display
         green.brightness = 0.8
         green.fill(0)
@@ -254,58 +351,23 @@ while True:
         time.sleep(2)
         green.fill(0)
         green.print(github_followers)
-        
-        print("\nAttempting to GET DISCORD SVG!")  # ------------------------------------------------
-        # Uncomment line below to print API URL with all data filled in
-        # print("Full API GET URL: ", ADA_DISCORD_SVG)
-        print("===============================")
-        try:
-            ada_SVG_response = requests.get(ADA_DISCORD_SVG).json()
-        except ConnectionError as e:
-            print("Connection Error:", e)
-            print("Retrying in 10 seconds")
-            
-        # Print Full JSON to Serial
-        full_ada_SVG_json_response = False # Change to true to see full response
-        if full_ada_SVG_json_response:
-            ada_SVG_dump_object = json.dumps(ada_SVG_response)
-            print("JSON Dump: ", ada_SVG_dump_object)
-            
-        # Print Debugging to Serial
-        ada_SVG_debug = True # Set to True to print Serial data
-        if ada_SVG_debug:
-            ada_SVG_users = ada_SVG_response['value']
-            print("SVG Value: ", ada_SVG_users)
-            regex = " online"
-            replace_with_nothing = ""
-            regex_users = re.sub(regex, replace_with_nothing, ada_SVG_users)
-            print("Regex Value: ", regex_users)
-        print("Monotonic: ", time.monotonic())
-        
-        # Discord SVG 7-Segment Display
-        white.brightness = 0.8
-        white.fill(0)
-        white.print("online")
-        time.sleep(2)
-        white.fill(0)
-        white.print(regex_users)
-        
+
         print("\nAttempting to GET DISCORD PREVIEW!")  # ------------------------------------------------
         # Uncomment line below to print API URL with all data filled in
-        print("Full API GET URL: ", ADA_DISCORD_SOURCE)
+        # print("Full API GET URL: ", ADA_DISCORD_SOURCE)
         print("===============================")
         try:
             ada_discord_response = requests.get(url=ADA_DISCORD_SOURCE, headers=discord_header).json()
         except ConnectionError as e:
             print("Connection Error:", e)
             print("Retrying in 10 seconds")
-            
+
         # Print Full JSON to Serial
         full_ada_discord_json_response = False # Change to true to see full response
         if full_ada_discord_json_response:
             ada_discord_dump_object = json.dumps(ada_discord_response)
             print("JSON Dump: ", ada_discord_dump_object)
-            
+
         # Print Debugging to Serial
         ada_discord_debug = True # Set to True to print Serial data
         if ada_discord_debug:
@@ -314,10 +376,10 @@ while True:
             ada_discord_all_members_online = ada_discord_response['approximate_presence_count']
             print("Online: ", ada_discord_all_members_online)
         print("Monotonic: ", time.monotonic())
-        
+
         # Discord 7-Segment Display
         white.fill(0)
-        white.print("total users")
+        white.print("subs")
         time.sleep(2)
         white.fill(0)
         white.print(ada_discord_all_members)
@@ -327,9 +389,9 @@ while True:
         time.sleep(2)
         white.fill(0)
         white.print(ada_discord_all_members_online)
-        
+
         print("\nFinished!")
-        print("Next Update in %s %s" % (int(sleep_int), sleep_time_conversion))
+        print("Next Update in: ", time_calc(sleep_time))
         print("===============================")
         gc.collect()
     # pylint: disable=broad-except
