51d2650199995b7c59206315db726750adaa837d
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-12-08 23:20:34 -0500

    fixed MCP addressing
    Fixed random crashing due to floating ground from MCP23017 address pads. Added example with ssd1306 display.

--- Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2/code_with_ssd1306.py
+++ Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2/code_with_ssd1306.py
@@ -6,12 +6,19 @@ import asyncio
 import time
 import board
 import busio
+import adafruit_displayio_ssd1306
+import displayio
+import terminalio
+from supervisor import ticks_ms
 from digitalio import Direction
 from adafruit_mcp230xx.mcp23017 import MCP23017
 from mcp23017_scanner import McpKeysScanner
 from multi_macropad import MultiKeypad
+from adafruit_display_text import label
 
-# Initialize 2 Separate Physical I2C busses
+displayio.release_displays()
+
+# Initialize 2 Separate Physical I2C buses
 i2c0 = busio.I2C(board.GP13, board.GP12)  # Bus I2C0
 i2c1 = busio.I2C(board.GP11, board.GP10)  # Bus I2C1
 
@@ -22,6 +29,14 @@ i2c1 = busio.I2C(board.GP11, board.GP10)  # Bus I2C1
 # I2C is definitely usable, only in that way.
 # Sincerely, DJIdiot
 """
+
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
+TEMPO = 120 # Beats Per Minute (approximation)
+
+display_bus = displayio.I2CDisplay(i2c1, device_address=0x3C)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HEIGHT)
 
 # Initialize MCP Chip 1 Step Switches 0-7
 mcp1 = MCP23017(i2c0, address=0x20)
@@ -63,6 +78,7 @@ def get_latch(mcp, pin):
 # if not, a conversion function could be used to translate:
 # (key_x, key_y) -> (led_x, led_y)
 
+BPM = TEMPO / 60 / 16
 # midi setup
 midi_tx_pin, midi_rx_pin = board.GP16, board.GP17
 midi_timeout = 0.01
@@ -73,15 +89,17 @@ async def blink_the_leds(delay=0.125):
     while True:
         # blink all the LEDs together
         for (x, y) in led_pins:
+            latched_pin = led_pins_per_chip[x][y]
             if not get_latch(x, y):
-                led_pins_per_chip[x][y].value = True
+                latched_pin.value = True
                 time.sleep(0.001)
-                led_pins_per_chip[x][y].value = False
+                latched_pin.value = False
                 await asyncio.sleep(delay)
-            else:
-                led_pins_per_chip[x][y].value = False
+            elif get_latch(x, y):
+                # print(latched_pin, latched_pin.value)
+                latched_pin.value = False
                 time.sleep(0.001)
-                led_pins_per_chip[x][y].value = True
+                latched_pin.value = True
                 await asyncio.sleep(delay)
 
 async def read_buttons():
@@ -93,18 +111,24 @@ async def read_buttons():
         while event := all_scanner.next_event():
             mcp_number = event.pad_number
             key_number = event.key_number
+            led_pin = led_pins_per_chip[mcp_number][key_number]
             if event.pressed:
-                print(f"Key pressed : {mcp_number} / {key_number}")
+                print(f"MCP : {mcp_number} / {key_number}")
                 # key pressed, find the matching LED
                 led_pin = led_pins_per_chip[mcp_number][key_number]
                 # invert the latch value (independently of the LED)
                 toggle_latch(mcp_number, key_number)
                 # change the LED value to match the latch
                 led_pin.value = get_latch(mcp_number, key_number)
+            if not event.pressed and led_pin.value:
+                led_pin.value = False
+                time.sleep(0.001)
+                led_pin.value = True
+
             # make sure to yield during the reading of the buttons
             await asyncio.sleep(0)
         # slow down the loop a little bit, can be adjusted
-        await asyncio.sleep(0.005)
+        await asyncio.sleep(BPM)
 
 async def main():
     await asyncio.gather(
