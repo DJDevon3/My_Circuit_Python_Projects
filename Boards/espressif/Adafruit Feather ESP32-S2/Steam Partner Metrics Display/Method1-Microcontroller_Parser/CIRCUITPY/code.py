d63a52e4bf4d26fb800f6011e690d6aba84b3020
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2023-04-20 22:12:04 -0400

    update CSV version
    Multiplexer no longer needed and general code cleanup

--- Boards/espressif/Adafruit Feather ESP32-S2/Steam Partner Metrics Display/Method1-Microcontroller_Parser/CIRCUITPY/code.py
+++ Boards/espressif/Adafruit Feather ESP32-S2/Steam Partner Metrics Display/Method1-Microcontroller_Parser/CIRCUITPY/code.py
@@ -2,26 +2,25 @@
 # SPDX-License-Identifier: MIT
 # Coded for Circuit Python 8.0
 # DJDevon3 Steam_CSV_Wishlist_Parser
+
 import os
 import time
 import board
 import busio
 import circuitpython_csv as csv
 from adafruit_ht16k33 import segments
-import adafruit_tca9548a
 
-# Reads a specifically named CSV file /CSV/SteamWishlists_xxxxxx_all.csv
-# File is updated daily by the batch script so it's always the same filename
-# with updated data and moved to this device's CSV directory.
+# Reads CSV file /CSV/SteamWishlists_xxxxxx_all.csv
+# File is updated daily by accompanying Windows batch script
+# Ensure batch script is pointing at USB drive letter for CIRCUITPY device
 
 Wishlist_Title = "xxxxxxx"  # Game appid
 CSV_directory = "/CSV/"  # directory for CSV's just in case
 
 # Initialize I2C 14-Segment Display
-i2c = board.I2C()
-display = segments.Seg14x4(board.I2C())
-tca = adafruit_tca9548a.TCA9548A(i2c)
-blue = segments.Seg14x4(tca[0], address=(0x71,0x72))
+i2c = board.STEMMA_I2C()
+blue = segments.Seg14x4(i2c, address=(0x71,0x72))
+blue.brightness = 0.5
 
 all_files = os.listdir(CSV_directory)  ## List all files in directory
 Wishlist_all = ("SteamWishlists_GameAppID_all.csv")  # Generic Demonstration
