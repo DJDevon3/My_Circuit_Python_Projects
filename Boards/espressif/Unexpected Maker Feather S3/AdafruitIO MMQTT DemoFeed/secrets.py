c4b9ea2ffc1c32074c297c1bb3181ab829ddc462
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2023-04-29 16:24:48 -0400

    add UM FeatherS3 MMQTT Example
    Used with DPS310 I2C sensor

---
+++ Boards/espressif/Unexpected Maker Feather S3/AdafruitIO MMQTT DemoFeed/secrets.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2019 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'xxxxxxxxxx',
+    'password' : 'xxxxxxxxxxx',
+    'aio_username' : 'xxxxxxxxxxxxx',
+    'aio_key' : 'xxxxxxxxxxxxxxxxxxxxx',
+    'broker' : 'io.adafruit.com',
+    'port' : 1883
+    }
