3af07af1d24fbb864d8861afe0dfefe7c559017d
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2023-09-16 14:31:30 -0400

    Add 192x128 RGB Matrix
    12 Panel RGB Matrix

---
+++ Boards/espressif/Adafruit MatrixPortal S3/192x128 RGB Matrix/README.md
@@ -0,0 +1,19 @@
+# 128x96 6-panel display
+
+### Hardware
+- 6x [64x32 5mm Pitch RGB Matrix Panels](https://www.adafruit.com/product/2277)
+- 1x [Adafruit MatrixPortal S3](https://www.adafruit.com/product/5778) (includes 1 20cm IDC cable per panel)
+- 1x [Adafruit BME688 I2C sensor module for temp, humidity, pressure, & VOC](https://www.adafruit.com/product/5046)
+- 2x [30cm IDC cable](https://www.amazon.com/dp/B07FZWH9S6)
+- 1x [5V 18A Meanwell PSU](https://www.amazon.com/dp/B018TEAPRQ)
+- 1x 3-prong (USA spec) PC power cord. Most people have an extra laying around. If you don't have one you'll need to source one.
+- 1x [AC Rocker Switch Socket](https://www.amazon.com/dp/B07RQV2NPN)
+- 1x [20A Fuses](https://www.amazon.com/dp/B0B1CPZ7XH)
+
+
+
+
+![IMG_1137](https://github.com/DJDevon3/My_Circuit_Python_Projects/assets/49322231/52ab4822-2fde-4b67-8f44-bdc12e0163d2)
+
+
+![IMG_1134](https://github.com/DJDevon3/My_Circuit_Python_Projects/assets/49322231/b0889211-a171-4b34-a2a6-b2c67033689a)
---
+++ Boards/espressif/Adafruit MatrixPortal S3/192x128 RGB Matrix/lib/adafruit_imageload/__init__.py
@@ -0,0 +1,92 @@
+# SPDX-FileCopyrightText: 2018 Scott Shawcroft for Adafruit Industries
+# SPDX-FileCopyrightText: 2022-2023 Matt Land
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_imageload`
+====================================================
+
+Load pixel values (indices or colors) into a bitmap and colors into a palette.
+
+* Author(s): Scott Shawcroft, Matt Land
+
+"""
+# pylint: disable=import-outside-toplevel
+
+try:
+    from typing import (
+        Tuple,
+        Iterator,
+        Optional,
+        List,
+        Iterable,
+        Union,
+    )
+    from io import BufferedReader
+    from displayio import Palette, Bitmap
+    from .displayio_types import PaletteConstructor, BitmapConstructor
+except ImportError:
+    pass
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ImageLoad.git"
+
+
+def load(
+    file_or_filename: Union[str, BufferedReader],
+    *,
+    bitmap: Optional[BitmapConstructor] = None,
+    palette: Optional[PaletteConstructor] = None
+) -> Tuple[Bitmap, Optional[Palette]]:
+    """Load pixel values (indices or colors) into a bitmap and colors into a palette.
+
+    bitmap is the desired type. It must take width, height and color_depth in the constructor. It
+    must also have a _load_row method to load a row's worth of pixel data.
+
+    palette is the desired palette type. The constructor should take the number of colors and
+    support assignment to indices via [].
+    """
+    if not bitmap or not palette:
+        try:
+            # use displayio if available
+            import displayio
+
+            if not bitmap:
+                bitmap = displayio.Bitmap
+            if not palette:
+                palette = displayio.Palette
+        except ModuleNotFoundError:
+            # meh, we tried
+            pass
+
+    if isinstance(file_or_filename, str):
+        open_file = open(file_or_filename, "rb")
+    else:
+        open_file = file_or_filename
+
+    with open_file as file:
+        header = file.read(3)
+        file.seek(0)
+        if header.startswith(b"BM"):
+            from . import bmp
+
+            return bmp.load(file, bitmap=bitmap, palette=palette)
+        if header.startswith(b"P"):
+            from . import pnm
+
+            return pnm.load(file, header, bitmap=bitmap, palette=palette)
+        if header.startswith(b"GIF"):
+            if not bitmap:
+                raise RuntimeError("bitmap argument required")
+
+            from . import gif
+
+            return gif.load(file, bitmap=bitmap, palette=palette)
+        if header.startswith(b"\x89PN"):
+            if not bitmap:
+                raise RuntimeError("bitmap argument required")
+            from . import png
+
+            return png.load(file, bitmap=bitmap, palette=palette)
+        raise RuntimeError("Unsupported image format")
